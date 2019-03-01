de2cd74ab1a1ffc20874ff23903ef3cce6a61fb0
Alexey Ryabov <ar@decast.com>
2019-03-01 15:05:38 +0300

    Implement get_courses_list

--- coursera.py
+++ coursera.py
@@ -1,10 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
 
 
-def get_courses_list():
-    pass
+def get_courses_list(max_size=20):
+    courses_url = "https://www.coursera.org/sitemap~www~courses.xml"
+    try:
+        response = requests.get(courses_url)
+    except requests.RequestException:
+        return None
+    xml_doc = BeautifulSoup(response.text, "xml")
+    urls = [url.loc.text for url in xml_doc.find_all('url')]
+    return urls[:max_size]
 
 
-def get_course_info(course_slug):
+def get_course_info(course_url):
     pass
 
 
@@ -12,5 +21,9 @@ def output_courses_info_to_xlsx(filepath):
     pass
 
 
-if __name__ == '__main__':
-    pass
+def main():
+    courses_urls = get_courses_list()
+
+
+if __name__ == "__main__":
+    main()
